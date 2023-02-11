e7d5204f2105c8e2c5b142942cad4b06b2815a6f
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2023-02-11 13:25:32 +0700

    Adjused background sizing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -33,8 +33,8 @@
 </template>
 <style scoped>
 .home {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     background-image: url("../assets/images/background.png");
     background-repeat: no-repeat;
     background-size: cover;
